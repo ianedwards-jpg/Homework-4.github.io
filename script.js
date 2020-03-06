867c713ba140667b2979901a92f2ce30c0e05732
Ian Edwards <57515741+ianedwards-jpg@users.noreply.github.com>
2020-03-06 12:26:50 -0500

    update

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var mainEl = document.getElementById("main");
 //var questArray = ["What does HTML stand for?", "What are the 3 building blocks of the web?", "What building block is responsible for a webpage's appearance?]
 
 //Start Div Variables 
-var startScreen = document.getElementById("startDiv")
+const startQuiz = document.getElementById("startQuiz")
 
 //Question and Button variables + Added event listeners
 const quiz = document.getElementById("#quiz") // Question Container 
@@ -14,56 +14,58 @@ const question = document.getElementById("#question") // Question Container
 const choiceA = document.getElementById("#choiceA").addEventListener("click", displayQuestion);
 const choiceB = document.getElementById("#choiceB").addEventListener("click", displayQuestion);
 const choiceC = document.getElementById("#choiceC").addEventListener("click", displayQuestion);
-const choiceD = document.getElementById("#choiceD").addEventListener("click", displayQuestion);
+const choiceD = document.getElementById("#choiceD" ).addEventListener("click", displayQuestion);
+
+const totalQuestions = questions.length - 1; //Reduces Questions array value from 7 to 6 
+
+//Questions Array 
+let questions = [
+  {
+  question: "What does HTML stand for?",
+  choiceA: "Hypotext Marking Language",
+  choiceB: "Hypertext Marker Language", 
+  choiceC: "Hypertext Markup Language", 
+  choiceD: "Hypotext Makeup Language",  
+  correct: "C"
+  },//End Question 1 
+  {
+  question: "What are the 3 building blocks of the web?",
+  choiceA: "HTML, CSS, JavaScript",
+  choiceB: "React, Angular, CSS", 
+  choiceC: "Hypertext Markup Language", 
+  choiceD: "HTML, Cobol, CSS",  
+  correct: "A"
+  },//End Question 2
+  {
+  question: "What building block is responsible for a webpage's styling?",
+  choiceA: "HTML",
+  choiceB: "BootStrap", 
+  choiceC: "JavaScript", 
+  choiceD: "CSS",  
+  correct: "D"
+  },//End Question 3
+]; 
 
 
-//Score Variables 
+//Score Variables
 var score = 0 
 
+//Timer Variables 
+const secondsLeft = 20;     //Quiz time 
+
+
 function displayQuestion(); {
-  //Function Variables 
+//Function Variables 
+let q = 
  document.getElementById("#question").innerHTML = question
  document.getElementById("#choiceA").innerHTML = choiceA
  document.getElementById("#choiceB").innerHTML = choiceB
  document.getElementById("#choiceC").innerHTML = choiceC
  document.getElementById("#choiceA").innerHTML = choiceD
 
-
-   let questions = [
-    {
-    question: "What does HTML stand for?",
-    choiceA: "Hypotext Marking Language",
-    choiceB: "Hypertext Marker Language", 
-    choiceC: "Hypertext Markup Language", 
-    choiceD: "Hypotext Makeup Language",  
-    correct: "C"
-    },//End Question 1 
-    {
-    question: "What are the 3 building blocks of the web?",
-    choiceA: "HTML, CSS, JavaScript",
-    choiceB: "React, Angular, CSS", 
-    choiceC: "Hypertext Markup Language", 
-    choiceD: "HTML, Cobol, CSS",  
-    correct: "A"
-    },//End Question 2
-    {
-    question: "What building block is responsible for a webpage's styling?",
-    choiceA: "HTML",
-    choiceB: "BootStrap", 
-    choiceC: "JavaScript", 
-    choiceD: "CSS",  
-    correct: "D"
-    },//End Question 3
-  ]; 
-
-  for (i = 0; i < questions.length; i++) {
-
-
-  }
-  
-
-
 }
+
+function 
 
 function answerIsCorrect()
 
@@ -74,7 +76,7 @@ function answerIsWrong()
 
 
 
-var secondsLeft = 20;     //Quiz time 
+
 
 
 var startButton = document.querySelector("#start");   //Start Button Connect to HTML
