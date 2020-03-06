a83888b64628d1e562e8116a0165de2665de3e57
Ian Edwards <ian.edward@uconn.edu>
2020-03-05 23:23:09 -0500

    update

--- script.js
+++ script.js
@@ -1,14 +1,58 @@
-var timeEl = document.querySelector(".time");
+var timeLeft = document.querySelector("time-left");
+var questDisplay = document.querySelector("question") //Assigns question variable to question div on webpage
 var mainEl = document.getElementById("main");
 
+
+var questArray = ["What does HTML stand for?", "What are the 3 building blocks of the web?", "What building block is responsible for a webpage's appearance?"]
 var startScreen = document.getElementById("startDiv")
 
-var secondsLeft = 10;     //Quiz time 
+
+funciton displayQuestion(); {
+  let questions = [
+    {
+    question: "What does HTML stand for?",
+    choiceA: "Hypotext Marking Language",
+    choiceB: "Hypertext Marker Language", 
+    choiceC: "Hypertext Markup Language", 
+    choiceD: "Hypotext Makeup Language",  
+    correct: "C"
+    },//End Question 1 
+    {
+    question: "What are the 3 building blocks of the web?",
+    choiceA: "HTML, CSS, JavaScript",
+    choiceB: "React, Angular, CSS", 
+    choiceC: "Hypertext Markup Language", 
+    choiceD: "HTML, Cobol, CSS",  
+    correct: "A"
+    },//End Question 2
+    {
+    question: "What building block is responsible for a webpage's styling?",
+    choiceA: "HTML",
+    choiceB: "BootStrap", 
+    choiceC: "JavaScript", 
+    choiceD: "CSS",  
+    correct: "D"
+    },//End Question 3
+  ]; 
+
+
+}
+
+function answerIsCorrect()
+
+
+function answerIsWrong()
+
+
+
+
+
+var secondsLeft = 20;     //Quiz time 
 
 function setTime() {
   var timerInterval = setInterval(function() {
     secondsLeft--;
-    timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
+    timeLeft.textContent = secondsLeft + " seconds left";
 
     if(secondsLeft === 0) {
       clearInterval(timerInterval);
@@ -17,3 +61,15 @@ function setTime() {
 
   }, 1000);
 }
+
+function sendMessage() {
+  timeEl.textContent = " ";
+
+  var imgEl = document.createElement("img");
+
+  imgEl.setAttribute("src", "images/image_1.jpg");
+  mainEl.appendChild(imgEl);
+
+}
+
+setTime();
