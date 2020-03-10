70c8851627aa9316e67bb366aa8f5666ee88a47e
Ian Edwards <ian.edward@uconn.edu>
2020-03-10 19:14:45 -0400

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 //Global Variables
-var timeLeft = document.querySelector("time-left");
-var questDisplay = document.querySelector("question") //Assigns question variable to question div on webpage
-var startButton = document.querySelector("startQuiz");   //Start Button Connect to HTML
+var timeLeft = document.querySelector("#time-left");
+var questDisplay = document.querySelector("#question") //Assigns question variable to question div on webpage
+var startButton = document.querySelector("#startQuiz");   //Start Button Connect to HTML
 var displayScore = document.getElementById("score")
 
 //Start Div Variables 
-const startQuiz = document.getElementById("startQuiz").addEventListener("click", startQuiz);
+const startQuiz = document.getElementById("startQuiz").addEventListener("click", quizLoop);
 
 //Question and Button variables + Added event listeners
 const quiz = document.getElementById("#quiz") // Question Container 
@@ -13,7 +13,7 @@ const question = document.getElementById("question") // Question Container
 const choiceA = document.getElementById("choiceA").addEventListener("click", checkAnswer);
 const choiceB = document.getElementById("choiceB").addEventListener("click", checkAnswer);
 const choiceC = document.getElementById("choiceC").addEventListener("click", checkAnswer);
-const choiceD = document.getElementById("choiceD" ).addEventListener("click", checkAnswer);
+const choiceD = document.getElementById("choiceD").addEventListener("click", checkAnswer);
 
 //Other Question Array
 var questions = ["What does HTML stand for?", "What are the 3 building blocks of the web?", "What building block is responsible for a webpage's styling?"]
@@ -21,60 +21,59 @@ var choicesA = ["Hypotext Marking Language", "HTML, CSS, JavaScript", "HTML"]
 var choicesB = ["Hypertext Marker Language", "Angular, HTML, JavaScript", "Python"]
 var choicesC = ["Python", "HTML, Python, JavaScript", "HTML, Python, JavaScript"]
 var choicesD = ["Hypotext Makeup Language", "Cobol, CSS, JavaScript", "CSS"]
-
+//Put Answers Array
+//first index = 
 //Score Variables
-var score = 0; displayScore = score; 
+var score = 0; displayScore = score;
 
 //Timer Variables 
 const secondsLeft = 20;     //Quiz time
 
 //Main Function - StartQuiz
-function startQuiz (); {
+function quizLoop() {
 
-//Timer Function
-function setTime() {
-    var timerInterval = setInterval(function() {
-      secondsLeft--;
-      timeLeft.textContent = secondsLeft + " seconds left";
- 
-      if(secondsLeft === 0) {
-        clearInterval(timerInterval);
-        //sendMessage();
-      }
- 
-    }, 1000);
-  }
- 
-//Question Cycle Function 
-function questionCycle(); {
+  //
   for (i = 0; i < questions.length; i++); {
-  console.log(list[i]) 
-  displayQuestion();
-  checkAnswer();
-
-  function displayQuestion(); {
-    //Function Variables 
-    let q = questions[questionCycle.result]
-    document.querySelector("question").innertext =  q.questions[i];
-    document.querySelector("choiceA").innerText = q.choicesA[i];
-    document.querySelector("choiceB").innerText = q.choicesB[i];
-    document.querySelector("choiceC").innerText = q.choicesC[i];
-    document.querySelector("choiceD").innerText = q.choicesD[i];
-        }
-
-  function checkAnswer (){
-    if(answer == question[questionCycle].correct)
-    //Increment score by 1 
-      score++
-    //
-    else{
-      secondsLeft = (secondsLeft - 5)
-        }
-      }
-    }
+    // console.log(list[i])
+    setTime();
+    displayQuestion(i);
+    checkAnswer();
   }
+}
 //Check Answer
-setTime();
+
+
+function displayQuestion(i) {
+  //Function Variables 
+  document.querySelector("#question").innertext = questions[i];
+  document.querySelector("#choiceA").innerText = choicesA[i];
+  document.querySelector("#choiceB").innerText = choicesB[i];
+  document.querySelector("#choiceC").innerText = choicesC[i];
+  document.querySelector("#choiceD").innerText = choicesD[i];
+}
+
+function setTime() {
+  var timerInterval = setInterval(function () {
+    secondsLeft--;
+    timeLeft.textContent = secondsLeft + " seconds left";
+
+    if (secondsLeft === 0) {
+      clearInterval(timerInterval);
+      //sendMessage();
+    }
+
+  }, 1000);
+}
+
+
+function checkAnswer() {
+  if (answer == question[questionCycle].correct)
+    //Increment score by 1 
+    score++
+  //
+  else {
+    secondsLeft = (secondsLeft - 5)
+  }
 }
 
 //var mainEl = document.getElementById("main");
@@ -156,7 +155,7 @@ setTime();
 //Timer Function and Variables 
 //var timerEl = document.querySelector("timer");
  //var mainEl = document.getElementById("main");
- 
+
 //  function sendMessage() {
 //    timerEl.textContent = " ";
 
@@ -167,4 +166,4 @@ setTime();
 
  //}
 
- 
+
